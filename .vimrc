524da154b6eed4329c39a53de1c786af61f14078
qdtang <qdtang.lance@gmail.com>
2017-09-25 18:30:04 +0800

    add .bash_aliases .vimrc & softwares.sh

---
+++ .vimrc
@@ -0,0 +1,32 @@
+"no compatible with vi
+set nocompatible
+
+"line number
+set nu
+
+"syntax
+syntax on
+
+"forbid temp file
+set nobackup
+set noswapfile
+
+"indent
+set autoindent
+set smartindent
+set cindent
+
+"tab
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+
+"search
+set hlsearch
+set incsearch
+
+"mouse
+set mouse=a
+
+"highlight match brackets
+set showmatch
---
+++ softwares.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+##################################################################
+# put "sudo" ahead of this script when you run it.
+##################################################################
+
+apt-get update
+apt-get upgrade 
+
+apt-get install vim
+apt-get install g++
+apt-get install libboost-all-dev
+apt-get install git
+
+apt-get install terminator
+apt-get install codeblocks
+apt-get install spyder
+#apt-get install spyder3
+apt-get install octave
