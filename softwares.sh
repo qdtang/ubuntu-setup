16e7276027b28f2af210cd9adda5bf96e9872358
qdtang <qdtang.lance@gmail.com>
2017-10-28 22:13:55 +0800

    add some softwares.

--- softwares.sh
+++ softwares.sh
@@ -7,13 +7,22 @@
 apt-get update
 apt-get upgrade 
 
+apt-get install build-essentail
+apt-get install terminator
 apt-get install vim
 apt-get install g++
 apt-get install libboost-all-dev
 apt-get install git
+apt-get install zeal
+apt-get install code
+apt-get install valgrind
+apt-get install qtcreator
 
-apt-get install terminator
-apt-get install codeblocks
+# install pip
+apt-get install python-pip python-dev
+pip install --upgrade pip
+pip install --upgrade virtualenv
+
 apt-get install spyder
 #apt-get install spyder3
-apt-get install octave
+#apt-get install octave
