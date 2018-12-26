7bc08df057ebc4fa1d4249887dc0d497fa30a1f7
qdtang <qdtang.lance@gmail.com>
2018-12-26 14:15:49 +0800

    rename function exit to exitWrapper

--- jumpdir.py
+++ jumpdir.py
@@ -49,7 +49,7 @@ def changeDir(path):
 		f.write("cd %s\n" % path)
 
 # both 0 and 1 mean success, but only 0 means "jump" is needed.
-def exit(status, updated, path=os.curdir):
+def exitWrapper(status, updated, path=os.curdir):
 	if updated:
 		dumpPaths()
 	if status == 0:
@@ -58,36 +58,38 @@ def exit(status, updated, path=os.curdir):
 
 def usage():
    print('''
-	   Please add following alias (for csh) to use this script:
-	   alias jd "/path/to/this/script \!* && source ~/.jumpdir.exe"
-	   Thus jd allows you to jump among directories via keywords in the path.
+##Usage:
+	Please add following alias for csh
+		alias jd "/path/to/this/script \!* && source ~/.jumpdir.exe"
+	Or, add a function for bash 
+		function jd(){/path/to/this/script $* && source ~/.jumpdir.exe}
+	After that jd allows you to jump among directories via keywords in the path.
 
-	   **Usage:
-	   jd -h|--help
+	jd -h|--help
 	   Display this help.
-	   jd -a [dir1 [, dir2...]]
+	jd -a [dir1 [, dir2...]]
 	   Add dirs to the quick access paths. If no dir is specified, it would add current directory.
-	   jd -r [dir1 [, dir2...]]
+	jd -r [dir1 [, dir2...]]
 	   Remove dirs from the quick access paths. If no dir is specified, it would remove current directory.
-	   jd -l
+	jd -l
 	   List the quick access paths.
-	   jd keyword1 [, keyword2...]
+	jd keyword1 [, keyword2...]
 	   cd to the already added directory specified by the keywords.
 	   Nothing happens if no directory is found.
 
-	   **Example:
-	   % jd -a /home/user/work/project/src /home/user/play/src    #add 2 dirs into quick access paths
-	   % jd wo src     #jump to /home/user/work/project/src
+##Example:
+	% jd -a /home/user/work/project/src /home/user/play/src    #add 2 dirs into quick access paths
+	% jd wo src     #jump to /home/user/work/project/src
 	''')
 
 def main():
 	if len(sys.argv) < 2:
 		usage()
-		exit(-1, False)
+		exitWrapper(-1, False)
 	arg1 = sys.argv[1]
 	if arg1 in ("-h", "--help"):
 		usage()
-		exit(1, False)
+		exitWrapper(1, False)
 	loadPaths()
 	updateFunc = {"-a":addPath, "-r":removePath}
 	if arg1 in ("-a", "-r"):
@@ -96,16 +98,16 @@ def main():
 		else:
 			for path in sys.argv[2:]:
 				updateFunc[arg1](path)
-		exit(1, True)
+		exitWrapper(1, True)
 	elif arg1 == "-l":
 		listPaths()
-		exit(1, False)
+		exitWrapper(1, False)
 	else:
 		path = findPath(sys.argv[1:])
 		if path == None:
-			exit(-1, False)
+			exitWrapper(-1, False)
 		else:
-			exit(0, False, path)
+			exitWrapper(0, False, path)
 
 
 if __name__ == "__main__":
