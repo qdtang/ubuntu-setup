50aec019b5aa350d56acab13ddfbe5a1476efd44
qdtang <qdtang.lance@gmail.com>
2020-08-13 22:30:02 -0700

    add clang tools and cppcheck

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ apt-get --yes install libgtest-dev
 apt-get --yes install git
 apt-get --yes install terminator
 apt-get --yes install cmake
+apt-get --yes install clang clang-format clang-tidy
+apt-get --yes install cppcheck
 apt-get --yes install valgrind
 apt-get --yes install zeal
 apt-get --yes install gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-tweaks
